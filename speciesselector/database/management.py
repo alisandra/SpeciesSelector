7091d71c8bfb5e17785b7c17750eee87d3374133
alisandra <alisandra.denton@hhu.de>
2022-01-09 20:50:40 +0100

    data and link prepping

--- speciesselector/database/management.py
+++ speciesselector/database/management.py
@@ -45,6 +45,8 @@ def add_species_from_tree(tree_path, sp_names, session, exact_match):
 
 
 class RoundHandler:
+    seed_str = 'seed'
+
     def __init__(self, session, split, id):
         self.session = session
         self.split = split
@@ -58,11 +60,22 @@ class RoundHandler:
             session.commit()
         else:
             raise ValueError(f"{len(rounds)} rounds found with id {id}???")
+        # setup basic directory
+        wdr = self.dir
+        for path in [wdr, os.path.join(wdr, 'training'), os.path.join(wdr, 'evaluation'),
+                     os.path.join(wdr, 'data')]:
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+    @property
+    def dir(self):
+        rtemp, stemp = 'round_{:03}', 'split_{:02}'
+        working_dir = self.session.query(orm.Path).filter(orm.Path.name == "working_dir").all()[0]
+        return os.path.join(working_dir, rtemp.format(self.id), stemp.format(self.split))
 
     def set_first_seeds(self, n_seeds=8):
         # get all species in set
-        set_sp = self.session.query(orm.Species).filter(orm.Species.split == self.split)
-        set_sp = [x for x in set_sp]
+        set_sp = self.session.query(orm.Species).filter(orm.Species.split == self.split).all()
         # select trainers
         random.shuffle(set_sp)
         trainers = set_sp[:n_seeds]
@@ -72,3 +85,53 @@ class RoundHandler:
         self.session.add(seed_model)
         self.session.add_all(seed_trainers)
         self.session.commit()
+
+    @property
+    def full_dir(self):
+        return self.session.query(orm.Path).filter(orm.Path.name == 'species_full').first()
+
+    @property
+    def subset_dir(self):
+        return self.session.query(orm.Path).filter(orm.Path.name == 'species_subset').first()
+
+    @property
+    def seed_model(self):
+        seed_model = self.session.query(orm.SeedModel).filter(orm.SeedModel.round_id == self.id).\
+            filter(orm.SeedModel.split == self.split).all()[0]
+        return seed_model
+
+    @property
+    def seed_training_species(self):
+        return [ssp.species for ssp in self.seed_model.seed_training_species]
+
+    @property
+    def seed_validation_species(self):
+        split_sp = self.session.query(orm.Species).filter(orm.Species.split == self.split).all()
+        return [sp for sp in split_sp if sp not in self.seed_training_species]
+
+    @property
+    def adj_training_dirs(self):
+        return ['adjustment_{}'.format(sp.name) for sp in self.seed_training_species]
+
+    def setup_data(self):
+        # setup training dir of seeds with 'full' h5 files
+        for dpath in [self.seed_str] + self.adj_training_dirs:
+            os.makedirs(dpath)
+
+        # setup seed data
+        seed_sp_t = self.seed_training_species
+        seed_sp_v = self.seed_validation_species
+        full_dir = self.full_dir
+        subset_dir = self.subset_dir
+        for sp in seed_sp_t:
+            os.symlink(
+                os.path.join(full_dir, sp.name, 'testing_data.h5'),
+                os.path.join(self.dir, 'data', self.seed_str, 'training_data.{}.h5'.format(sp.name)))
+        for sp in seed_sp_v:
+            os.symlink(
+                os.path.join(subset_dir, sp.name, 'testing_data.h5'),
+                os.path.join(self.dir, 'data', self.seed_str, 'validation_data.{}.h5'.format(sp.name))
+            )
+        # setup adjusted data
+
+
